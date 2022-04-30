6a9144266919c5264dedb72a5b3df1a0045d30cc
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-04-30 23:41:05 +0300

    create model folder

---
+++ app/model/news.py
@@ -0,0 +1,9 @@
+class News:
+    def __init__(self,name,author,image,title,description,creation):
+        self.name = name
+        self.author = author
+        self.image = image
+        self.title = title
+        self.description = description
+        self.creation = creation
+            
