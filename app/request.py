871ae17db8569562b4042be0887ce29a7fe3a66c
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-01 22:44:15 +0300

    create the get headlines method

--- app/request.py
+++ app/request.py
@@ -1,7 +1,24 @@
 from app import app
-import urllib.request.json
+import urllib.request,json
 from .model import news
 
+News = news.News
 
 api_key = app.config['NEWS_API_KEY']
 base_url = app.config['HEADLINES_API_BASE_URL']
+
+def get_headlines():
+    get_headlines_url = base_url.format(api_key)
+    
+    with urllib.request.urlopen(get_headlines_url) as url:
+        get_headlines_data = url.read()
+        get_headlines_response = json.load(get_headlines_data)
+        
+        headlines_results = None
+        
+        if get_headlines_response['totalResults']:
+            headlines_results_list = get_headlines_response['totalResults']
+            headlines_results = process_results(headlines_results_list)
+            
+        return headlines_results
+    
