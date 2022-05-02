379486ee04c66c2b4aa17317177ef78c129d1ff1
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-02 15:28:24 +0300

    create sources class

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config:
 
     
     HEADLINES_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-    SOURCES_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    SOURCES_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
     
 
 class ProdConfig(Config):
