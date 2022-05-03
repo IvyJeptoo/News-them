404701a130e5fecd5b0e97686237fd6e521d4f9b
IvyJeptoo <ivy.jeptoo@student.moringaschool.com>
2022-05-03 11:01:03 +0300

    deployment to heroku

---
+++ app/news_test.py
@@ -0,0 +1,35 @@
+import unittest
+from model import Headlines, Sources
+
+
+class HeadlinesTest(unittest.TestCase):
+    '''
+    test to test the new headline class behaviour
+    '''
+    def setUp(self):
+        '''
+        set up method that will run before every test
+        '''
+        self.new_Headline = Headlines('Amina Hassan','imani','Arabia Queens','Fall of the lovely three sisters','www.queen.com','www.urltoimage','Published at:2022-05-02T14:15:00Z')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_Headline,Headlines))
+        
+
+class Sources (unittest.TestCase):
+    '''
+    test to test new source behaviour
+    '''
+    def setUp(self):
+        '''
+        set up method that will run before every test
+        '''
+        self.new_Source = Sources('CNN', 'Russian ukraine war to keep on going','www.cnn.com')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_Source, Sources))
+        
+        
+if __name__ =='__main__':
+    unittest.main()
+    
